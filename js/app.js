ad4678b74973a1749e34e89cfb719ec6236480f2
Ignacio Galdames <caminantestelar@gmail.com>
2016-03-29 10:21:19 -0300

    añadir variable compañero
    añadir variable compañero,  variable numero accion jugar y cambios en
    nombres de variables

--- js/app.js
+++ js/app.js
@@ -4,10 +4,11 @@ $(document).foundation();
 
 // #Variables
 var turnoJugador = ["yo", "izquierda", "derecha", "todos", "hombres", "mujeres", "viejo", "joven", "alto", "bajo"];
-var accionJugador = ["trago", "nunca", "limon", "dedo", "regla", "qlh", "vom", "cultura", "shot", "cascada", "historia", "piquito", "beso"];
+var accionJugador = ["trago", "nunca", "limon", "dedo", "regla", "qlh", "vom", "cultura", "shot", "cascada", "historia", "piquito", "beso","compañero"];
 var decisionJugador = ["elijo", "eligen"];
+var duracionAccion = ["turno", "siempre"];
 var complemento_accionJugador = ["dar", "recibir"];
-var numero_accionJugador = [1, 2, 3];
+var numeroAccionJugador = [1, 2, 3];
 
 
 // IDEA: si sale penitencia como valor, entonces cargar una penitencia
@@ -17,12 +18,31 @@ var mensaje = function getMessage() {
 
   // Random vars
   var valorTurnoJugador = turnoJugador[Math.floor(Math.random() * turnoJugador.length)];
+    // Variables Acciones
+  if (valorTurnoJugador === "todos" || valorTurnoJugador === "hombres" || valorTurnoJugador === "mujeres") {
+    accionJugador = ["trago","regla","shot","piquito","beso"];
+  }
   var valorAccionJugador = accionJugador[Math.floor(Math.random() * accionJugador.length)];
   var valorDecisionJugador = decisionJugador[Math.floor(Math.random() * decisionJugador.length)];
   var valorComplementoAccionJugador = complemento_accionJugador[Math.floor(Math.random() * complemento_accionJugador.length)];
-  var valorNumeroAccionJugador = numero_accionJugador[Math.floor(Math.random() * numero_accionJugador.length)];
+  var valorNumeroAccionJugador = numeroAccionJugador[Math.floor(Math.random() * numeroAccionJugador.length)];
+  var valorDuracionAccion = duracionAccion[Math.floor(Math.random() * duracionAccion.length)];
 
-  // Variables Acciones
+  // plural Acciones
+  if (valorNumeroAccionJugador > 1) {
+    pluralAcciones = ["s"];
+  }
+  else {
+    pluralAcciones = [" "];
+  }
+
+  // Duración de la Acción
+  if (valorDuracionAccion === "turno") {
+      textoValorDuracionAccion = ["durante" + " " + valorNumeroAccionJugador + " " + "turno" + pluralAcciones];
+  }
+   else if (valorDuracionAccion === "siempre") {
+      textoValorDuracionAccion = ["por siempre jamás"];
+  }
 
   // Iconos acción juego
   if (valorAccionJugador === "tomar") {
@@ -47,7 +67,7 @@ var mensaje = function getMessage() {
       textoAccionJugador = ["decir una verdad o una mentira"];
   }
   else if (valorAccionJugador === "cultura") {
-        textoAccionJugador = ["comments"];
+        textoAccionJugador = ["debe empezar un tema para jugar al bachillerato" ];
   }
   else if (valorAccionJugador === "shot") {
       textoAccionJugador = ["tomar un shot"];
@@ -64,15 +84,18 @@ var mensaje = function getMessage() {
   else if (valorAccionJugador === "beso") {
       textoAccionJugador = ["dar un beso"];
   }
+  else if (valorAccionJugador === "compañero") {
+      textoAccionJugador = ["debe elegir a alguien para que haga lo mismo" + " " + textoValorDuracionAccion];
+  }
 
   // Variables lenguage
   if (valorTurnoJugador === "todos" || valorTurnoJugador === "hombres" || valorTurnoJugador === "mujeres") {
-    plural = ["s"];
+    pluralJugadores = ["s"];
   }
   else {
-    plural = [" "];
+    pluralJugadores = [" "];
   }
-  var conjugar = [" " + valorTurnoJugador + " " + "le" + plural + " "+ "toca"];
+  var conjugar = [" " + valorTurnoJugador + " " + "le" + pluralJugadores + " "+ "toca"];
 
   // Variables turno jugador
   if (valorTurnoJugador === "yo") {
