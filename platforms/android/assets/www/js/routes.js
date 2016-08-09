e18fcdbe3d3a3c933e9d44fcbef6e6c9e9eee2aa
Ignacio Galdames <caminantestelar@gmail.com>
2016-08-09 11:09:37 -0400

    añadir android

---
+++ platforms/android/assets/www/js/routes.js
@@ -0,0 +1,53 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('inicio.jugar', {
+    url: '/jugar',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/jugar.html',
+        controller: 'jugarCtrl'
+      }
+    }
+  })
+
+  .state('inicio.jugadores', {
+    url: '/jugadores',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/jugadores.html',
+        controller: 'jugadoresCtrl'
+      }
+    }
+  })
+
+  .state('inicio', {
+    url: '/inicio',
+    templateUrl: 'templates/inicio.html',
+    abstract:true
+  })
+
+  .state('inicio.nuevoJugador', {
+    url: '/nuevo_jugador',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/nuevoJugador.html',
+        controller: 'nuevoJugadorCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/inicio/jugar')
+
+  
+
+});
